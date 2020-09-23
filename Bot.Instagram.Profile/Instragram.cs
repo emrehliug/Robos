8b44e5e516bc2b393bdebdee5d8f689af2c75ff6
unknown <guigui410@gmail.com>
2020-09-23 16:49:58 -0300

    iniciando bot para obter perfil do instagram

---
+++ Bot.Instagram.Profile/Instragram.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Bot.Instagram.Profile
+{
+    static class Instragram
+    {
+        public static Profile GetProfileByUser(string userName)
+        {
+            var profile = new Profile(userName);
+
+            string url = @"https://www.instagram.com/" + userName + "/?hl=pt-br";
+            string html;
+
+            using (WebClient wc = new WebClient())
+            {
+                html = wc.DownloadString(url);
+            }
+
+            HtmlAgilityPack.HtmlDocument conteudoHtml = new HtmlAgilityPack.HtmlDocument();
+            conteudoHtml.LoadHtml(html);
+
+            return profile;
+        }
+    }
+}
+  
