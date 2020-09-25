b6a4886a10b7b6c9f540e459ba73601ccf8efc3e
unknown <guigui410@gmail.com>
2020-09-25 14:47:44 -0300

    concluido robo que raspa perfil no instagram

--- Bot.Instagram.Profile/Program.cs
+++ Bot.Instagram.Profile/Program.cs
@@ -6,7 +6,20 @@ namespace Bot.Instagram.Profile
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Profile perfil;
+            Console.WriteLine("Digite o perfil que deseja pesquisar: ");
+
+            if ((perfil = Instragram.GetProfileByUser(Console.ReadLine())) != null)
+            {
+                Console.WriteLine($"Nome = {perfil.UserName}");
+                Console.WriteLine($"Descricao = {perfil.Description}");
+                Console.WriteLine($"Link para o perfil = {perfil.Url}");
+            }
+            else
+            {
+                Console.WriteLine("Perfil não encontrado!\nTente novamente com perfil valido.");
+            }
+            
 
             Console.ReadKey();
         }
